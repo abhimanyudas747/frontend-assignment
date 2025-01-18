83870a0c0ee4606f3e65a9fb0d01faba0ba6f9d6
Abhimanyu Das <60252577+abhimanyudas747@users.noreply.github.com>
2025-01-18 21:55:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import './App.css';
+
+const ITEMS_PER_PAGE = 5;
+
+function App() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json')
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+  }, []);
+  const maxPage = useMemo(() => {
+    return Math.ceil(data.length / ITEMS_PER_PAGE);
+  }, [data]);
+  const isFirstPage = useMemo(() => {
+    return page === 1;
+  }, [page]);
+  const isLastPage = useMemo(() => {
+    return page === maxPage;
+  }, [page, maxPage]);
+  const handleNext = useCallback(() => {
+    if (page + 1 <= maxPage) {
+      setPage(page + 1);
+    }
+  }, [page, maxPage]);
+  const handlePrev = useCallback(() => {
+    if (page - 1 >= 1) {
+      setPage(page - 1);
+    }
+  }, [page]);
+
+  const start = useMemo(() => (page - 1) * ITEMS_PER_PAGE, [page]);
+  const end = useMemo(() => page * ITEMS_PER_PAGE);
+  return (
+    <div className="App">
+      <div className='title-div'>
+        <h1>Highly-rated Kickstarter Projects</h1>
+      </div>
+      <div className='table-div'>
+      <table data-testid="table">
+        <thead>
+          <tr>
+            <th>
+              S.no
+            </th>
+            <th>
+            Percentage funded
+            </th>
+            <th>
+            Amount pledged
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          data.slice(start, end).map(item => {
+            return (
+              <tr key={item["s.no"]}>
+                <td>{item["s.no"]}</td>
+                <td>{item["percentage.funded"]}</td>
+                <td>{item["amt.pledged"]}</td>
+              </tr>
+            )})
+        }
+        </tbody>
+      </table>
+      </div>
+      <div className="pagination">
+        <button data-testid="prev-btn" onClick={handlePrev} disabled={isFirstPage}>Prev</button>
+        <button data-testid="next-btn" onClick={handleNext} disabled={isLastPage}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/tests/App.spec.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from '../App';
+import { FETCH_SUCCESS } from './mocks';
+
+// mock fetch for testing
+
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve(FETCH_SUCCESS),
+    })
+  );
+})
+
+
+
+
+test('renders title', async () => {
+  render(<App />);
+  const titleElement = screen.getByText("Highly-rated Kickstarter Projects");
+  await waitFor(() => {
+    expect(titleElement).toBeInTheDocument();
+  })
+  
+});
+
+test('renders prev and next buttons', async () => {
+  render(<App />);
+  const prevBtn = await screen.findByTestId("prev-btn");
+  const nextBtn = await screen.findByTestId("next-btn");
+  await waitFor(() => {
+    expect(prevBtn).toBeInTheDocument();
+    expect(nextBtn).toBeInTheDocument();
+  });
+});
+
+test('renders table', async () => {
+  render(<App />);
+  const table = await screen.findByTestId("table");
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  
+});
+
+test('renders table rows', async () => {
+  render(<App />);
+  const table = await screen.findByTestId("table");
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  const rows = table.querySelectorAll('tr');
+  expect(rows.length).toBe(6);
+});
+
+test('renders table rows with correct data', async () => {
+  render(<App />);
+  const table = await screen.findByTestId("table");
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  const rows = table.querySelectorAll('tr');
+  const firstRow = rows[1];
+  const cells = firstRow.querySelectorAll('td');
+  expect(cells[0].textContent).toBe('0');
+  expect(cells[1].textContent).toBe('186');
+  expect(cells[2].textContent).toBe('15823');
+});
+
+test('clicking next and prev button should change page', async () => {
+  render(<App />);
+  
+  const table = await screen.findByTestId("table");
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  const nextBtn = await screen.findByTestId("next-btn");
+  nextBtn.click();
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  const rows = table.querySelectorAll('tr');
+  const firstRow = rows[1];
+  const cells = firstRow.querySelectorAll('td');
+  expect(cells[0].textContent).toBe('5');
+  expect(cells[1].textContent).toBe('114');
+  expect(cells[2].textContent).toBe('2065');
+  const prevBtn = await screen.findByTestId("prev-btn");
+  prevBtn.click();
+  await waitFor(() => {
+    expect(table).toBeInTheDocument();
+  })
+  const newRows = table.querySelectorAll('tr');
+  const newFirstRow = newRows[1];
+  const newCells = newFirstRow.querySelectorAll('td');
+  expect(newCells[0].textContent).toBe('0');
+  expect(newCells[1].textContent).toBe('186');
+  expect(newCells[2].textContent).toBe('15823');
+});
